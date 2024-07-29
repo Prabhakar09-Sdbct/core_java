82238a8904eccb16c210fc59c69f3baf6b81f9c9
Prabhakar Mandloi <prabhakarmandloi@gmail.com>
2024-07-29 20:26:33 +0530

    29-07-2024

--- src/com/pm/corejava/string/VowelCount.java
+++ src/com/pm/corejava/string/VowelCount.java
@@ -3,8 +3,7 @@ package com.pm.corejava.string;
 public class VowelCount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 System.out.println("working");
 	}
 
 }
